c00670f9727645fc514bd1dee825136e870c52c6
ryanponti <ryanakbar.armial@gmail.com>
2017-10-07 17:40:40 +0700

    new Home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -26,8 +26,16 @@ class HomeController extends Controller
      */
     public function index(){
 
-      $bands = Band::orderBy('created_at', 'desc')->take(8)->get();
+      // $bands = Band::orderBy('created_at', 'desc')->take(8)->get();
+      $bands = DB::table('songs')
+              ->select('bands.band_name', 'songs.judul', 'bands.foto', 'songs.id')
+              ->join('bands', 'bands.id', '=', 'songs.band_id')
+              ->where('songs.score', '>=', 69)
+              ->inRandomOrder()
+              ->take(8)
+              ->get();
 
+      // return $bands;
       // return redirect()->to('/submission');
       return view('index', ['page' => 'home', 'bands' => $bands]);
     }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,15 +11,15 @@
     <img class="responsive-img hide-on-small-only" src="{{ asset('images/home-logo.png') }}">
     <img class="hide-on-med-and-up responsive-img" src="{{ asset('images/home-logo.png') }}">
   </div>
-  <div class="btn-cta">
+  <!-- <div class="btn-cta">
     <ul>
       <li><a href="{{ route('vote') }}">VOTE NOW</a></li>
-      <!-- <li><a href="{{ route('submission') }}">SUBMIT NOW</a></li> -->
+      <li><a href="{{ route('submission') }}">SUBMIT NOW</a></li>
     </ul>
-    <!-- <ul class="btn-pop-head">
+    <ul class="btn-pop-head">
       <li><a href="{{ route('popup') }}">LEVI'S&#174; POP UP STUDIO</a></li>
-    </ul> -->
-  </div>
+    </ul>
+  </div> -->
 </div>
 <div class="wrap-text margin-bottom-50">
   <div class="container">
@@ -31,12 +31,65 @@
     <div id="band-hunt" class="row">
       <div class="col s12">
         <div class="judul-page">
-          APA SIH Levi's&#174; BAND HUNT?
+          40 Band Semifinalis
         </div>
       </div>
-      <div class="col s12">
-        <div class="isi-page">
-          Levi’s&#174; Band Hunt adalah audisi digital pertama untuk berburu band-band muda terbaik di Indonesia. Mengambil filosofi "Authentic Self Expression", kampanye ini bertujuan untuk mencari band independent dengan lagu asli dari genre apapun yang paling unik dan otentik di seluruh Indonesia. Kini saatnya tunjukkan pada seluruh Indonesia kalau band kamu memiliki keunikan tersendiri, berbeda dari yang lain. Raih kesempatan jadi band beken dan mendapatkan record deal dari Universal Music Indonesia, uang tunai, dan juga paket produk dari Levi’s&#174; untuk seluruh anggota band!
+      <div id="list-vote" class="col s12">
+        <div class="col s12 m6 l3">
+          <ol>
+            <li>Alena</li>
+            <li>Banana Project</li>
+            <li>Batiga</li>
+            <li>Beauty And The Beats</li>
+            <li>Black Drama</li>
+            <li>By The Way</li>
+            <li>Cheery Trees</li>
+            <li>Coffee Strikes</li>
+            <li>Dewantara Rock</li>
+            <li>DPR</li>
+          </ol>
+        </div>
+        <div class="col s12 m6 l3">
+          <ol start="11">
+            <li>Dynamic Trio</li>
+            <li>Fly To the Moon</li>
+            <li>Good News</li>
+            <li>Jeane</li>
+            <li>Lada</li>
+            <li>Lorong Waktu</li>
+            <li>Mary Celeste</li>
+            <li>Moluska</li>
+            <li>Noy</li>
+            <li>Own Band</li>
+          </ol>
+        </div>
+        <div class="col s12 m6 l3">
+          <ol start="21">
+            <li>Q-Oz_ZeroFive</li>
+            <li>Rival & Friends</li>
+            <li>Romantick</li>
+            <li>Ruang Kosong</li>
+            <li>Shade</li>
+            <li>Sinthink</li>
+            <li>Stigma</li>
+            <li>Teori</li>
+            <li>The Godspeed</li>
+            <li>The Little Lizard</li>
+          </ol>
+        </div>
+        <div class="col s12 m6 l3">
+          <ol start="31">
+            <li>The Wise</li>
+            <li>Tutur Nada</li>
+            <li>Violet Fighter</li>
+            <li>Visual Dream Band</li>
+            <li>Ep’s</li>
+            <li>Evvi</li>
+            <li>Soloensis</li>
+            <li>Tetra</li>
+            <li>The Istiqomah</li>
+            <li>You!</li>
+          </ol>
         </div>
       </div>
     </div>
